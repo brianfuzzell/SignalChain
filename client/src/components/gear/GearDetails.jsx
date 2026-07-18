466174e1c23b1e65eb08c013476a4e345fd94a71
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-07-18 18:52:33 -0500

    Added handler, methods, fetch for Gear Type

--- client/src/components/gear/GearDetails.jsx
+++ client/src/components/gear/GearDetails.jsx
@@ -1,49 +1,120 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Button, Form, FormGroup, Input, Label } from "reactstrap"
-
+import { useNavigate, useParams } from "react-router-dom";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import { getGearById, getGears } from "../../managers/gearManager";
+import { getGearTypes } from "../../managers/gearTypeManager";
+import { getGearTypeById } from "../../managers/gearTypeManager";
 
 export const GearDetails = () => {
-    const { id } = useParams();
-    const [gear, setGear] = useState(null);
+  const { id } = useParams();
+  const [gears, setGears] = useState([]);
+  const [gear, setGear] = useState(null);
+  const [gearTypes, setGearTypes] = useState([]);
+  const [gearType, setGearType] = useState(null);
+  const [formData, setFormData] = useState({
+    gearTypeId: "",
+    model: "",
+    purchaseYear: "",
+    quantity: "",
+    serialNumber: "",
+  });
+  const navigate = useNavigate();
 
-    return (
-        <>
-            <h2>Gear Details</h2>
-            <h4>{gear.model}</h4>
-            <Form>
-                <FormGroup>
-                    <Label for="type">
-                        Type
-                    </Label>
-                    <Input type="text" value="Type" id="type"/>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="model">
-                        Model
-                    </Label>
-                    <Input type="text" value="Model" id="model"/>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="purchase-year">
-                        Purchase Year
-                    </Label>
-                    <Input type="text" value="Purchase Year" id="purchase-year"/>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="quantity">
-                        Quantity
-                    </Label>
-                    <Input type="text" value="Quantity" id="quantity"/>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="serial-number">
-                        Serial Number
-                    </Label>
-                    <Input type="text" value="Serial Number" id="serial-number"/>
-                </FormGroup>
-                <Button type="submit">Update</Button>
-            </Form>
-        </>
-    )
-}
+  const getGearDetails = (id) => {
+    getGearById(id).then(setGear);
+  };
+
+  useEffect(() => {
+    getGears()
+      .then(setGears);
+  }, []);
+
+  useEffect(() => {
+    getGearById(id).then((fetchedGear) => {
+      setGear(fetchedGear);
+      setFormData({
+        gearTypeId: fetchedGear.gearTypeId,
+        model: fetchedGear.model,
+        purchaseYear: fetchedGear.purchaseYear,
+        quantity: fetchedGear.quantity,
+        serialNumber: fetchedGear.serialNumber,
+      });
+    });
+  }, [id]);
+
+  useEffect(() => {
+    getGearTypes()
+      .then(setGearTypes);
+  }, []);
+
+  if (gear === null) return <p>Gear item not found.</p>;
+
+  return (
+    <>
+      <h2>Gear Details</h2>
+      <h4>{gear.model}</h4>
+      <Form>
+        <FormGroup>
+          <Label for="type">Type</Label>
+          {/* TODO: dropdown should be pre-selected to this item's type */}
+          <select
+            value={formData.gearType}
+            onChange={(e) => setFormData({ ...formData, type: e.target.value })}
+          >
+            <option value="">Gear type</option>
+            {gearTypes.map((gt) => (
+              <option key={gt.id} value={gt.id}>
+                {gt.name}
+              </option>
+            ))}
+          </select>
+        </FormGroup>
+        <FormGroup>
+          <Label for="model">Model</Label>
+          <Input
+            type="text"
+            value={formData.model}
+            id="model"
+            onChange={(e) =>
+              setFormData({ ...formData, model: e.target.value })
+            }
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="purchase-year">Purchase Year</Label>
+          <Input
+            type="text"
+            value={formData.purchaseYear}
+            id="purchase-year"
+            onChange={(e) =>
+              setFormData({ ...formData, purchaseYear: e.target.value })
+            }
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="quantity">Quantity</Label>
+          <Input
+            type="text"
+            value={formData.quantity}
+            id="quantity"
+            onChange={(e) =>
+              setFormData({ ...formData, quantity: e.target.value })
+            }
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="serial-number">Serial Number</Label>
+          <Input
+            type="text"
+            value={formData.serialNumber}
+            id="serial-number"
+            onChange={(e) =>
+              setFormData({ ...formData, serialNumber: e.target.value })
+            }
+          />
+        </FormGroup>
+        <Button type="submit">Update</Button>
+      </Form>
+    </>
+  );
+};
