043ad2b0f9121722e1716b73e9baae3770a25189
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-07-21 17:05:35 -0500

    WIP Adding song details page

--- client/src/components/songs/SongDetails.jsx
+++ client/src/components/songs/SongDetails.jsx
@@ -1,11 +1,42 @@
-
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { getSongById } from "../../managers/songManager";
 
 export const SongDetails = () => {
+  const { id } = useParams();
+  const [songs, setSongs] = useState([]);
+  const [song, setSong] = useState(null);
+  const [formData, setFormData] = useState({
+    title: "",
+    writer: "",
+    artist: "",
+    yearRecorded: "",
+    statusId: "",
+  });
+  const navigate = useNavigate();
 
+  const getSongDetails = (id) => {
+    getSongById(id).then(setSong);
+  };
 
-    return (
-        <>
-            <h2>Song Details</h2>
-        </>
-    )
-}
+  useEffect(() => {
+    getSongById(id).then((fetchedSong) => {
+      setSong(fetchedSong);
+      setFormData({
+        title: fetchedSong.title,
+        writer: fetchedSong.writer,
+        artist: fetchedSong.artist,
+        yearRecorded: fetchedSong.yearRecorded,
+        statusId: fetchedSong.statusId,
+      });
+    });
+  }, [id]);
+
+  if (song === null) return <p>Song not found.</p>;
+
+  return (
+    <>
+      <h2>Song Details</h2>
+    </>
+  );
+};
