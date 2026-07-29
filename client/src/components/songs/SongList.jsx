14fb6bd2ef71a2c07dc7fc7d120b2717347c3cec
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-07-29 11:55:27 -0500

    Made Songs list width consistent with Gear list

--- client/src/components/songs/SongList.jsx
+++ client/src/components/songs/SongList.jsx
@@ -19,7 +19,7 @@ export const SongList = ({ loggedInUser }) => {
   };
 
   return (
-    <div className="container container-padding" style={{ maxWidth: "700px" }}>
+    <div className="container container-padding" style={{ maxWidth: "800px" }}>
       <div className="hdr-btn-wrapper">
         <div className="hdr-btn-h2">
           <h2>Songs in Production</h2>
